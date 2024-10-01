11816009288ba7979cc66002c62116214b647f5b
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-10-01 18:00:00 +0700

    Add Layout UI (include header and footer)

---
+++ app/views/layout/header.php
@@ -0,0 +1,26 @@
+<header id="header">
+  <div id="header-content">
+    <nav class="main-nav">
+      <div class="logo">
+        <img src="/shopping/assets/images/Exclusive.png" alt="" srcset="">
+      </div>
+      <ul class="nav">
+        <li class="nav-item"><a href="" class="nav-link">Home</a></li>
+        <li class="nav-item"><a href="" class="nav-link">Contact</a></li>
+        <li class="nav-item"><a href="" class="nav-link">About</a></li>
+        <li class="nav-item"><a href="" class="nav-link">Sign Up</a></li>
+      </ul>
+    </nav>
+    <div class="actions">
+      <form action="" method="post" id="search">
+        <input type="text" placeholder="What are you looking for?" />
+        <img src="/shopping/assets/icons/icon-search.svg" alt="" srcset="">
+      </form>
+      <div class="carts">
+        <img src="/shopping/assets/icons/icon-wishlist.svg" alt="" srcset="">
+        <img src="/shopping/assets/icons/icon-cart.svg" alt="" srcset="">
+        <img src="/shopping/assets/icons/icon-user.svg" alt="" srcset="">
+      </div>
+    </div>
+  </div>
+</header>

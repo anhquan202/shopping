0f9db43bcda1e6817363508de9506d9334876f81
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-10-07 23:03:21 +0700

    Add and initialize CategoriesModel and ProductModel in construct

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,10 +1,18 @@
 <?php
+require_once __DIR__ . '/../models/CategoriesModel.php';
+require_once __DIR__ . '/../models/ProductModel.php';
 class HomeController{
+  private $categoriesModel, $productModel;
   public function __construct() {
-
+    $this->categoriesModel = new CategoriesModel(null, null);
+    $this->productModel = new ProductModel();
   }
 
   public function index() {
-
+    ob_start();
+    $categories = $this->categoriesModel->getCategories();
+    require_once __DIR__ . '/../views/home/index.php';
+    $content = ob_get_clean();
+    require_once __DIR__ . '/../views/layout/index.php';
   }
 }

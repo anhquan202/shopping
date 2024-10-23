ac7e50367fac8f6350988fc340e438d9b1172104
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-10-23 23:41:18 +0700

    Create eav-product folder and add models as product entity, attributes and values

---
+++ app/models/eav-product/FriendlyLabels.php
@@ -0,0 +1,31 @@
+<?php
+class FriendlyLabels
+{
+  public static function getLabels()
+  {
+    return [
+      'color' => 'Color',
+      'camera' => 'Camera',
+      'screen' => 'Screen Size',
+      'operating_system' => 'Operating System',
+      'ram' => 'RAM',
+      'rom' => 'ROM',
+      'processor' => 'Processor',
+      'cpu_speed' => 'CPU Speed',
+      'cpu_core' => 'CPU Cores',
+      'cpu_thread' => 'CPU Threads',
+      'storage_capacity' => 'Storage Capacity',
+      'storage_type' => 'Storage Type',
+      'graphics_card' => 'Graphics Card',
+      'pin' => 'Battery',
+      'charging_power' => 'Charging Power',
+      'sound_technology' => 'Sound Technology',
+      'charging_type' => 'Charging Type',
+      'battery_life ' => 'Battery Life',
+      'charging_time' => 'Charging Time',
+      'dimension' => 'Dimension',
+      'weight' => 'Weight',
+      'manufacturer' => 'Manufacturer'
+    ];
+  }
+}

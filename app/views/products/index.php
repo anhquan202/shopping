a1a86a194b24b77c07e7ae4d1ce46ef9f273e93c
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-10-23 22:08:13 +0700

    Update UI for Product Page

--- app/views/products/index.php
+++ app/views/products/index.php
@@ -1,7 +1,5 @@
 <?php $title = 'Our Products';
 
 require_once __DIR__ . '/../components/arrange.php';
-require_once __DIR__ . '../../components/product-list.php';
-require_once __DIR__ . '../../components/pagination.php';
-
-?>
+require_once __DIR__ . '/../components/product-list.php';
+require_once __DIR__ . '/../components/pagination.php';

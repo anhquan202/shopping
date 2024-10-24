c2a36f38556dbe15cb7f405834df4a55a354ced2
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-10-24 09:46:04 +0700

    Create Layout for Product Detail Page

---
+++ app/views/productDetail/overview-product.php
@@ -0,0 +1,31 @@
+<div class="overview-product">
+  <div class="image-info">
+    <img src="/shopping/assets/images/<?= $productById['primary_image'] ?>" alt=" Main product image" class="image-main">
+  </div>
+
+  <div class="summary-info">
+    <h1 class="name"><?= ucfirst($productById['name']) ?></h1>
+    <div class="price">
+      <span>Cost </span>
+      <span><?= number_format($productById['price_out']) . ' vnđ' ?></span>
+    </div>
+
+    <div class="quantity-product">
+      <span class="title">Quantity</span>
+      <div class="quantity-input">
+        <button class="btn-update btn-minus">
+          <img src="/shopping/assets/icons/minus-solid.svg" alt="">
+        </button>
+        <input type="number" id="quantity" name="quantity" value="1" min="1" max="<?= $productById['stock'] ?>" step="1">
+        <button class="btn-update btn-plus">
+          <img src="/shopping/assets/icons/plus-solid.svg" alt="">
+        </button>
+      </div>
+      <span class="stock"><?= $productById['stock'] . ' Valid Products' ?></span>
+    </div>
+    <div class="actions">
+      <button class="btn btn_add-to-cart" data-product-id="<?= $productById['product_id'] ?>">Add to Cart</button>
+      <button class="btn btn_buy">Buy Now</button>
+    </div>
+  </div>
+</div>

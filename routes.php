dad29dbcd0c5bce7b2b13f31e1f3291689abba14
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-10-28 11:26:05 +0700

    Add Page routes and APIs

--- routes.php
+++ routes.php
@@ -1,4 +1,13 @@
 <?php
 $routes = [
   '' => 'HomeController@index',
+  'products' => 'ProductController@index',
+  'productDetail' => 'ProductDetailController@index',
+  'cart' => 'CartController@index',
+
+  //api route
+  'cart/add-to-cart' => 'CartController@addToCart',
+  'cart/counter-cart' => 'CartController@counterCart',
+  'cart/remove-item' => 'CartController@removeItem',
+  'cart/get-total-price' => 'CartController@getTotalPrice'
 ];

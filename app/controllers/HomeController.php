2f8dd8be653a7f79f7291e23bd8e3952f5263933
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-10-28 16:16:59 +0700

    Change ProductModel path and format code

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,14 +1,17 @@
 <?php
 require_once __DIR__ . '/../models/CategoriesModel.php';
-require_once __DIR__ . '/../models/ProductModel.php';
-class HomeController{
+require_once __DIR__ . '/../models/eav-product/ProductModel.php';
+class HomeController
+{
   private $categoriesModel, $productModel;
-  public function __construct() {
+  public function __construct()
+  {
     $this->categoriesModel = new CategoriesModel(null, null);
     $this->productModel = new ProductModel();
   }
 
-  public function index() {
+  public function index()
+  {
     ob_start();
     $categories = $this->categoriesModel->getCategories();
     $products = $this->getRandomProducts();
@@ -17,7 +20,8 @@ class HomeController{
     require_once __DIR__ . '/../views/layout/index.php';
   }
 
-  public function getRandomProducts (){
+  public function getRandomProducts()
+  {
     return $this->productModel->getRandomProducts();
   }
 }

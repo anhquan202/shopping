3b9ca95a3f8e8518cf609433b9fcc92e68731017
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-11-04 11:41:48 +0700

    Create API for Register user

--- app/controllers/AuthController.php
+++ app/controllers/AuthController.php
@@ -26,4 +26,41 @@ class AuthController
     $content = ob_get_clean();
     require_once __DIR__ . '/../views/layout/index.php';
   }
+  public function register()
+  {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+      $full_name = $_POST['full_name'] ?? null;
+      $user_phone = $_POST['user_phone'] ?? null;
+      $password = $_POST['password'] ?? null;
+      $repeat_password = $_POST['repeat_password'] ?? null;
+
+      $data = [
+        'full_name' => $full_name,
+        'user_phone' => $user_phone,
+        'password' => $password,
+        'repeat_password' => $repeat_password
+      ];
+
+      $result = $this->userModel->register($data);
+
+      if ($result === true) {
+        $_SESSION['user'] = [
+          'full_name' => $full_name,
+          'user_phone' => $user_phone,
+        ];
+        header('Content-Type: application/json');
+        echo json_encode([
+          'success' => 201,
+          'message' => 'Register successfully',
+        ]);
+      } else {
+        header('Content-Type: application/json');
+        echo json_encode([
+          'success' => 409,
+          'message' => 'Register failed',
+          'errors' => $result,
+        ]);
+      }
+    }
+  }
 }

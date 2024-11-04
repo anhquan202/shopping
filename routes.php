67413339e0029217d76f08900eecd60d38ae1926
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-11-04 11:42:28 +0700

    Update routes

--- routes.php
+++ routes.php
@@ -4,10 +4,12 @@ $routes = [
   'products' => 'ProductController@index',
   'productDetail' => 'ProductDetailController@index',
   'cart' => 'CartController@index',
-
+  'login' => 'AuthController@loginPage',
+  'signup' => 'AuthController@signupPage',
   //api route
   'cart/add-to-cart' => 'CartController@addToCart',
   'cart/counter-cart' => 'CartController@counterCart',
   'cart/remove-item' => 'CartController@removeItem',
-  'cart/get-total-price' => 'CartController@getTotalPrice'
+  'cart/get-total-price' => 'CartController@getTotalPrice',
+  'register' => 'AuthController@register'
 ];

f11fa5e5b2f2862f37291f059cd1d4088a5f9a8b
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-11-09 22:23:59 +0700

    Update: import getInForUser components

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ import counter from './components/counter';
 import showHideLabel from './components/showHideLabel';
 import register from './auth/register';
 import signin from './auth/signin';
-import getInforUser from './components/userTokenHandler';
+import { getInforUser } from './components/userTokenHandler';
 $(function () {
   automaticSlide();
 

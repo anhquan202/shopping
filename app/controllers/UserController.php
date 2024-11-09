968ccd65b711a7df0379ee0a998bee539aad7b8a
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-11-09 22:15:23 +0700

    Update: create getUserInfo function

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,2 +1,24 @@
 <?php
-class UserController {}
+require_once __DIR__ . '/../models/JWTModel.php';
+class UserController
+{
+  private $jwtModel;
+  public function __construct()
+  {
+    $this->jwtModel = new JWTModel();
+  }
+  public function getUserInfo()
+  {
+    if (isset($_COOKIE['auth_token'])) {
+      $jwt_token = $_COOKIE['auth_token'];
+      try {
+        $user = $this->jwtModel->decodeToken($jwt_token);
+        echo json_encode($user);
+      } catch (\Throwable $th) {
+        echo json_encode([
+          'message' => $th->getMessage()
+        ]);
+      }
+    }
+  }
+}

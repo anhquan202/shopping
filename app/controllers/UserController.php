393f303f53d2135b49f0980fb228b32e0439ebcd
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-12-01 01:41:54 +0700

    Update: json response

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -9,22 +9,33 @@ class UserController
   }
   public function getUserInfo()
   {
-    if (isset($_COOKIE['auth_token'])) {
-      $jwt_token = $_COOKIE['auth_token'];
-      try {
+    try {
+      if (isset($_COOKIE['auth_token'])) {
+        $jwt_token = $_COOKIE['auth_token'];
         $user = $this->jwtModel->decodeToken($jwt_token);
 
+        http_response_code(200);
         header('Content-Type: application/json');
         echo json_encode([
           'status' => 201,
           'user' => $user
         ]);
-      } catch (\Throwable $th) {
+      } else {
+        http_response_code(400);
         header('Content-Type: application/json');
         echo json_encode([
-          'message' => $th->getMessage()
+          'status' => 400,
+          'message' => 'User is not authenticated'
         ]);
       }
+    } catch (\Throwable $th) {
+      http_response_code(500);
+      header('Content-Type: application/json');
+      echo json_encode([
+        'status' => 500,
+        'message' => $th->getMessage(),
+        'trace' => $th->getTraceAsString(),
+      ]);
     }
   }
 }

77f144dd6a1abba068efea9ecb27a0e87772bfd7
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-12-04 15:30:40 +0700

    Update: - get user from model - json reponse

--- app/controllers/UserController.php
+++ app/controllers/UserController.php
@@ -1,25 +1,37 @@
 <?php
 require_once __DIR__ . '/../models/JWTModel.php';
+require_once __DIR__ . '/../models/eav-users/UserModel.php';
 class UserController
 {
-  private $jwtModel;
+  private $jwtModel, $userModel;
   public function __construct()
   {
     $this->jwtModel = new JWTModel();
+    $this->userModel = new UserModel();
   }
   public function getUserInfo()
   {
     try {
       if (isset($_COOKIE['auth_token'])) {
         $jwt_token = $_COOKIE['auth_token'];
-        $user = $this->jwtModel->decodeToken($jwt_token);
-
-        http_response_code(200);
-        header('Content-Type: application/json');
-        echo json_encode([
-          'status' => 201,
-          'user' => $user
-        ]);
+        $decode = $this->jwtModel->decodeToken($jwt_token);
+        $user_id = $decode->data->user_id;
+        $user = $this->userModel->getInfoUser($user_id);
+        if ($user) {
+          http_response_code(200);
+          header('Content-Type: application/json');
+          echo json_encode([
+            'status' => 201,
+            'user' => $user
+          ]);
+        } else {
+          http_response_code(404);
+          header('Content-Type: application/json');
+          echo json_encode([
+            'status' => 404,
+            'message' => 'User not found'
+          ]);
+        }
       } else {
         http_response_code(400);
         header('Content-Type: application/json');

e2db48fa5f289bac5e9d547d8fbcef8cc6ac8d8b
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-12-25 23:34:37 +0700

    Update: modal UI ( change information user before checkout)

--- app/views/checkout/index.php
+++ app/views/checkout/index.php
@@ -1,80 +1,106 @@
-<?php $title = 'Checkout Page' ?>
+<?php $title = 'Checkout Page';
+$total_price = 0;
+foreach ($carts['products'] as $product) {
+  $total_price += $product['quantity'] * $product['price_out'];
+}
+?>
 <form id="checkout">
   <div class="billing-details">
     <h2>Billing Details</h2>
-    <div class="info-user">
-      <div class="form-group">
-        <label for="full_name">Full Name *</label>
-        <input type="text" id="full_name" name="full_name" class="input-field" placeholder="Full Name" required>
+    <div class="receiver-address">
+      <h3>Receiver Address</h3>
+      <div class="info-detail">
+        <div class="identity">
+          <span id="full_name"></span>
+          <span id="user_phone"></span>
+          <span id="address"></span>
+        </div>
+        <a href="#" class="change-address">Thay đổi</a>
       </div>
-
-      <div class="form-group">
-        <label for="phone">Phone Number*</label>
-        <input type="tel" id="phone" name="phone" class="input-field" placeholder="Phone" required>
-      </div>
-
-      <div class="form-group">
-        <label for="address">Address*</label>
-        <input type="text" id="address" name="address" class="input-field" placeholder="Receiver Address" required>
-        <div id="suggestions-list" class="suggestions">
-          <div class="options">
-            <div id="province" class="btn-select active">Province</div>
-            <div id="district" class="btn-select">District</div>
-            <div id="ward" class="btn-select">Ward</div>
+      <div id="modal-overlay">
+        <div id="modal">
+          <h3>User Information</h3>
+          <div class="info-user">
+            <div class="form-group">
+              <label for="full_name">Full Name *</label>
+              <input type="text" id="full_name" name="full_name" placeholder="Full Name" class="input-field">
+              <b class="error error-fullname"></b>
+            </div>
+            <div class="form-group">
+              <label for="user_phone">Phone Number *</label>
+              <input type="text" id="user_phone" name="user_phone" placeholder="Phone Number" class="input-field">
+              <b class="error error-phone"></b>
+            </div>
           </div>
-          <ul class="list-address">
+          <div class="form-group">
+            <label for="address">Address*</label>
+            <input type="text" id="address" name="address" class="input-field" placeholder="Receiver Address" required>
+            <div id="suggestions-list" class="suggestions">
+              <div class="options">
+                <div id="province" class="btn-select active">Province</div>
+                <div id="district" class="btn-select">District</div>
+                <div id="ward" class="btn-select">Ward</div>
+              </div>
+              <ul class="list-address">
 
-          </ul>
+              </ul>
+            </div>
+          </div>
         </div>
       </div>
-
-
-      <div>
-        <input type="checkbox" id="save-info" name="save-info">
-        <label for="save-info">Save this information for faster check-out next time</label>
-      </div>
-    </div>
-  </div>
-
-  <div class="order-summary">
-    <h2>Order Summary</h2>
-    <div class="order-items">
-      <div class="item">
-        <img src="assets\images\Headphone\bluetooth-airpods-2-apple-mv7n2-imei-ava.jpg" alt="LCD Monitor">
-        <span class="name">LCD Monitor</span>
-        <span class="price_out">$650</span>
-      </div>
-      <div class="item">
-        <img src="assets\images\Headphone\bluetooth-airpods-2-apple-mv7n2-imei-ava.jpg" alt="HI Gamepad">
-        <span class="name">HI Gamepad</span>
-        <span class="price_out">$1100</span>
-      </div>
     </div>
 
-    <div class="order-totals">
-      <div class="subtotal">
-        <span>Subtotal:</span>
-        <span>$1750</span>
+    <div class="order-summary">
+      <h2>Order Summary</h2>
+      <table class="order-items">
+        <thead>
+          <tr>
+            <th>Image</th>
+            <th>Product Name</th>
+            <th>Unit Price</th>
+            <th>Quantity</th>
+            <th>Subtotal</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php foreach ($carts['products'] as $product): ?>
+            <tr class="item">
+              <td><img src="/shopping/assets/images/<?= $product['primary_image'] ?>" alt="product-image"></td>
+              <td><span><?= $product['name']; ?></span></td>
+              <td><span class="price_out">
+                  <?= number_format($product['price_out']); ?>
+                </span></td>
+              <td><span><?= $product['quantity']; ?></span></td>
+              <td><span class="products-price" id="subtotal"><?= number_format($product['quantity'] * $product['price_out']); ?></span></td>
+            </tr>
+          <?php endforeach; ?>
+        </tbody>
+      </table>
+
+      <div class="order-totals">
+        <div class="total-amount">
+          <strong class="amount" name="amount"><?= number_format($total_price); ?></strong>
+        </div>
       </div>
-      <div class="shipping-cost">
-        <span>Shipping:Free</span>
-      </div>
-      <div class="total-amount">
-        <strong>Total: $1750</strong>
-      </div>
+      <input type="hidden" name="amount" value="<?= floatval($total_price); ?>">
     </div>
 
     <div class="payment-options">
-      <label for="">
-        <input type="radio" name="payment-method" value="vnpay" required>
-        <img src="assets\icons\icon-vnpay.png" alt="VNPay" class="icon-payment">
-      </label>
-      <label for="">
-        <input type="radio" name="payment-method" value="cod" required>
-        Cash on delivery
-      </label>
+      <h3>Payment Method</h3>
+      <section class="options">
+        <label for="vnpay">
+          <input type="radio" name='payment-method' value="vnpay" required>
+          <img src="assets\icons\icon-vnpay.png" alt="VNPay" class="icon-payment">
+        </label>
+        <label for="cod">
+          <input type="radio" name='payment-method' value="cod" required>
+          Cash on delivery
+        </label>
+      </section>
     </div>
 
-    <button type="submit">Place Order</button>
+    <div class="purchase">
+      <button type="submit" data-cart-id="<?= $carts['cart_id'] ?>" name="redirect" class="btn-purchase">Place Order</button>
+    </div>
   </div>
 </form>

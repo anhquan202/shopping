ca9a1d47d651160ef36b9964caa2c4d04d36644e
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2024-12-31 16:42:37 +0700

    Update: add userId in addToCart function

--- js/components/cart.js
+++ js/components/cart.js
@@ -1,71 +1,81 @@
 import $, { error } from 'jquery';
+import { getUserInfoFromApi } from './handleUserInfo';
 import { updateTotalPrice } from './counter';
 function handleCart() {
   addToCart();
-  updateCartCount();
   removeItem();
 }
 
 function addToCart() {
-  $('.btn_add-to-cart').on('click', function (e) {
+  $('.btn_add-to-cart').on('click', async function (e) {
     e.preventDefault();
     var productId = $(this).data('product-id');
     var quantity = $('.quantity').val();
-
-    $.ajax({
-      method: "POST",
-      url: 'cart/add-to-cart',
-      data: {
-        product_id: productId,
-        quantity: quantity
-      },
-      success: function (response) {
-        if (response.success === 200) {
-          alert(response.message);
-          updateCartCount();
-        } else {
-          alert(response.message);
-          window.location.href = 'login';
+    try {
+      const user = await getUserInfoFromApi();
+      const userId = user.user_id;
+      $.ajax({
+        method: "POST",
+        url: 'cart/add-to-cart',
+        data: {
+          user_id: userId,
+          product_id: productId,
+          quantity: quantity
+        },
+        success: function (response) {
+          if (response.status === 200) {
+            alert(response.message);
+            updateCartCount();
+          }
+        },
+        error: function (error) {
+          console.log(error);
         }
-      },
-      error: function (jqXHR, textStatus, errorThrown) {
-        console.log("AJAX request failed: " + textStatus + ", " + errorThrown);
-      }
-    })
+      })
+    } catch (error) {
+      alert(error.message);
+      window.location.href = 'login';
+    }
   })
 }
 
 //handle counter cart and show in cart-icon
-function updateCartCount() {
-  $.ajax({
-    method: "POST",
-    url: 'cart/counter-cart',
-    success: function (response) {
-      if (response.success === 200) {
-        $('#shopping-cart').text(response.total_items);
-      } else {
-        console.log('Error fetching cart count');
+export function updateCartCount() {
+  try {
+    $.ajax({
+      method: "POST",
+      url: 'cart/counter-cart',
+      success: function (response) {
+        if (response.status === 200) {
+          $('#shopping-cart').text(response.total_items);
+        } else {
+          console.log('Error fetching cart count');
+        }
+      },
+      error: function (xhr) {
+        console.log(xhr.responseText);
       }
-    },
-    error: function (jqXHR, textStatus, errorThrown) {
-      console.log("AJAX request failed: " + textStatus + ", " + errorThrown);
-    }
-  });
+    });
+  } catch (xhr) {
+    console.log(xhr);
+  }
 }
 
 function removeItem() {
   $('.btn-remove').on('click', function (e) {
     e.preventDefault();
-    var productId = $(this).data('product-id');
+    const cartId = $('#carts').data('cart-id');
+    const productId = $(this).data('product-id');
     if (confirm('Remove this product?')) {
       $.ajax({
         method: "POST",
         url: 'cart/remove-item',
         data: {
-          product_id: productId
+          product_id: productId,
+          cart_id: cartId
         },
         success: function (response) {
-          if (response.success === 200) {
+          if (response.status === 200) {
             $('#product-' + productId).remove();
             if ($('.list-item .item').length === 0) {
               $('.my-carts').html('<p>Your cart is empty. Add items to start shopping!</p>');
@@ -79,7 +89,7 @@ function removeItem() {
         }
       })
     }
-    updateTotalPrice();
   })
+  updateTotalPrice();
 }
 export default handleCart;

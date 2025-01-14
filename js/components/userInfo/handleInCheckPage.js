f3298a68eb969dd5f0cbc64b713d12791a55ad2d
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-01-14 23:18:05 +0700

    Update: function to handle changing user information before billing

--- js/components/userInfo/handleInCheckPage.js
+++ js/components/userInfo/handleInCheckPage.js
@@ -1,20 +1,55 @@
 import $ from 'jquery';
 import { getUserInfoDetail } from './getUserInfoFromApi';
+import { validatePhone } from '../validate/validatePhone';
+import { validateFullName } from '../validate/validateFullName';
+let receiverName = '', receiverPhone = '', receiverAddress = '';
+function handleInCheckPage() {
+  showInfoInCheckPage();
 
+  changeUserInfo();
+}
 async function showInfoInCheckPage() {
   try {
     const userInfo = await getUserInfoDetail();
-    const fullName = capitalizeFirstLetter(userInfo.fullName)
-    $('#receiver-name').text(fullName);
-    $('#receiver-phone').text(userInfo.userPhone);
-    $('#receiver-address').text(userInfo.info.address);
+    const fullName = capitalizeFirstLetter(userInfo.fullName);
+    receiverName = fullName;
+    receiverPhone = userInfo.userPhone;
+    receiverAddress = userInfo.info.address;
 
-    $('.form-group').find('#full_name').val(fullName);
-    $('.form-group').find('#user_phone').val(userInfo.userPhone);
-    $('.form-group').find('#address').val(userInfo.info.address);
+    $('.form-group').find('#full_name').val(receiverName);
+    $('.form-group').find('#user_phone').val(receiverPhone);
+    $('.form-group').find('#address').val(receiverAddress);
+    updateUI();
   } catch (error) {
     console.log(error);
   }
+}
+
+function changeUserInfo() {
+  validate();
+  $('.btn-save').on('click', function () {
+    receiverName = $('#full_name').val();
+    receiverPhone = $('#user_phone').val();
+    receiverAddress = $('#address').val();
+    if (validateFullName() && validatePhone()) {
+      $('#modal-overlay').css('display', 'none');
+      updateUI();
+    }
+    // $('#modal-overlay').css('display', 'none');
+  });
+}
+function validate() {
+  $('#full_name').on('input blur', function () {
+    validateFullName();
+  });
+  $('#user_phone').on('input blur', function () {
+    validatePhone();
+  });
+}
+function updateUI() {
+  $('#receiver-name').text(receiverName);
+  $('#receiver-phone').text(receiverPhone);
+  $('#receiver-address').text(receiverAddress);
 }
 
 function capitalizeFirstLetter(str) {
@@ -28,4 +63,4 @@ function capitalizeFirstLetter(str) {
       .join(' ');
   }
 }
-export { showInfoInCheckPage };
+export { handleInCheckPage };

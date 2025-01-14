0c369a502198aea7559304b1ea38b9553acd7f88
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-01-14 23:20:57 +0700

    Refactor: remove redundant components

--- js/pages/checkout.js
+++ js/pages/checkout.js
@@ -1,11 +1,9 @@
-import $ from 'jquery';
 import getAddressData from "../components/address/handleProvinceData";
-import { showInfoInCheckPage } from '../components/userInfo/handleInCheckPage';
+import { handleInCheckPage } from '../components/userInfo/handleInCheckPage';
 import { modal } from "../components/modal";
-import { showInfoInCheckPage } from '../components/userInfo/handleInCheckPage';
 import { purchase } from '../components/checkout';
 export default function checkout() {
-  showInfoInCheckPage();
+  handleInCheckPage();
   getAddressData();
   modal();
   purchase();

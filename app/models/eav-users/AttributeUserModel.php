b4f2f1650aa4248496a54c7602bed38f26922ff5
AnhQuan202 <111556330+anhquan202@users.noreply.github.com>
2025-01-24 00:34:11 +0700

    Update: get attribute fields

--- app/models/eav-users/AttributeUserModel.php
+++ app/models/eav-users/AttributeUserModel.php
@@ -3,10 +3,26 @@ require_once __DIR__ . '/../../../config/connDatabase.php';
 
 class AttributeUserModel
 {
-  private $attribute_id, $attribute_name;
-  public function __construct($attribute_id = null, $attribute_name = null)
+  private $conn;
+  public function __construct()
   {
-    $this->attribute_id = $attribute_id;
-    $this->attribute_name = $attribute_name;
+    $db = new connDatabase();
+    $this->conn = $db->getConnection();
+  }
+  public function getUserAttribute()
+  {
+    $query = 'SELECT * FROM user_attributes';
+    $stmt = $this->conn->prepare($query);
+    $result_map = [];
+    if ($stmt->execute()) {
+      $result = $stmt->get_result();
+      while ($row = $result->fetch_assoc()) {
+        $result_map[] = [
+          'attribute_name' => $row['attribute_name'],
+          'attribute_id' => $row['attribute_id']
+        ];
+      }
+    }
+    return $result_map;
   }
 }
